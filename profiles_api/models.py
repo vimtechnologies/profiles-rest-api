e423e005314de1606dc203b385dd5c8c9fbc4718
vincenzo.mangini <vincenzomangini@outlook.com>
2020-04-05 14:23:44 +0200

    Add and enable app in Django Setting file

---
+++ profiles_api/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+# out of the box django comes with the default user model that is used with the standard authentication system and also the django admin
+# we are going to override this with our custom module that allows to use an email address instead of the standar username that comes
+# with standard django model. Below are the imports you need to add
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+# under this we need to create a new class called user profile and inherit from AbstractUser and PermissionsMixin
+# Create your models here.
+
+class UserProfile(AbstractUser, PermissionsMixin):
+    """Database model for user in the system""" # python standard for writing docstrings
+    email = models.EmailField(max_length=255, unique=True)
+    # the above line email means we want an email column on our userprofile database and we want this column field filled with max 255 character and
+    # unique= True means two users can not use same email address so each user is unique
+    name = models.CharField(max_length=255)
+    # the above allow 255 character for the name field
+    is_active = models.BooleanField(default=True)
+    # the above line is used to determine if a user is active or not - by default we set it to true
+    is_staff = models.BooleanField(default=False)
+    # the above determine if the user is a staff user which is used to determine if the user should have access to the django admin - in this case by default all user are not django admin
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email' # this allow to change from defaul username authentication to email
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return  self.name
+
+    def __str__(self):
+        """Return string rappresentation of our user"""
+        return self.email #reccomended for all django model
+
