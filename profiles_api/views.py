c0730c094ed47ee0d4b180a8c4eec64a36c9b855
vincenzo.mangini <vincenzomangini@outlook.com>
2020-04-11 13:06:18 +0200

    views.py file update

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test Api View"""
+    serializer_class = serializers.HelloSerializers
 
     def get(self, request, format=None):
         """Returns a list of APIview features"""
@@ -15,3 +18,21 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+
+
+
+
+
+
+
+
+
+
